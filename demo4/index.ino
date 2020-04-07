b1f01c0ab7048df0215263a8247519742f66c795
codemobiles <chaiyasit.t@gmail.com>
2020-04-07 16:01:59 +0700

    2134

--- demo4/index.ino
+++ demo4/index.ino
@@ -1,12 +1,16 @@
 #include <ESP8266WiFi.h>
 #define WIFI_SSID "cm_guest"
 #define WIFI_PASSWORD "87654321"
+String LINE_TOKEN ="017KwJGa0tY8i3URv3CLdKL66qytVcE0h7NZJEDlHIZ";
 
+
+// DHT11 Demo
 void setup()
 {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     getWiFiIPAddress();
+    sendLineNotification("CodeMobiles Ready...");
 }
 
 void loop()
@@ -26,4 +30,46 @@ void getWiFiIPAddress()
     Serial.println();
     Serial.print("connected: ");
     Serial.println(WiFi.localIP());
+}
+
+
+
+void sendLineNotification(String message) {
+
+  String msg = String("message=") + message;
+
+  WiFiClientSecure client;
+  if (!client.connect("notify-api.line.me", 443)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String req = "";
+  req += "POST /api/notify HTTP/1.1\r\n";
+  req += "Host: notify-api.line.me\r\n";
+  req += "Content-Type: application/x-www-form-urlencoded\r\n";
+  req += "Authorization: Bearer " + String(LINE_TOKEN) + "\r\n";
+  req += "Content-Length: " + String(msg.length()) + "\r\n";
+  req += "\r\n";
+  req +=  msg;
+
+  client.print(req);
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  
+  Serial.println("closing connection");
 }
