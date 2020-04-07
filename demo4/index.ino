cd7ecb1b8663d7e439e959ba24be7a8ec32f8b1b
codemobiles <chaiyasit.t@gmail.com>
2020-04-07 15:14:09 +0700

    2134

---
+++ demo4/index.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#define WIFI_SSID "cm_guest"
+#define WIFI_PASSWORD "87654321"
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    getWiFiIPAddress();
+}
+
+void loop()
+{
+}
+
+void getWiFiIPAddress()
+{
+    // connect to wifi.
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
+}
