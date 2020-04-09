6d14fc6be94f1253311a798f009dfb85c46c8d09
codemobiles <chaiyasit.t@gmail.com>
2020-04-09 10:00:10 +0700

    2134

--- demo6/index.ino
+++ demo6/index.ino
@@ -2,19 +2,37 @@
 #define WIFI_SSID "cm_guest"
 #define WIFI_PASSWORD "87654321"
 
+// Infrared
+#define IRsensor 5 // 5 = D1, 4 = D2
+long lastTimestamp = 0;
+int count = 0;
+
 void setup()
 {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     getWiFiIPAddress();
 
+    // infrared
+    pinMode(IRsensor, INPUT_PULLUP);
+    attachInterrupt(IRsensor, react, CHANGE);
 }
 
 void loop()
 {
-    
 }
 
+//When interrupt orrcured, activate react function.
+void react()
+{
+    long currentTimestamp = millis();
+    if ((currentTimestamp - lastTimestamp) > 200)
+    {
+        count++;
+        Serial.println(count);
+    }
+    lastTimestamp = currentTimestamp;
+}
 
 void getWiFiIPAddress()
 {
